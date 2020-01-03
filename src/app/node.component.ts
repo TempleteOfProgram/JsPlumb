c6650c324f962d503fa7fcf126125347d6b486c2
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2020-01-03 12:13:13 -0500

    box shadow added

--- src/app/node.component.ts
+++ src/app/node.component.ts
@@ -35,7 +35,7 @@ export interface Node {
                 height: 100px;
                 padding: 4px;
                 text-align: center;
-                border-radius: 50%;
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
                 }`]
 })
 
